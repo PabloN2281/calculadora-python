3c9ac3b3449dca2b6b0b9a5b985f1f468adb24fe
PabloN2281 <147391197+PabloN2281@users.noreply.github.com>
2024-09-30 18:29:20 -0300

    Update views.py
    deixando mais descritivo

--- app/views.py
+++ app/views.py
@@ -7,19 +7,19 @@ def calculator_view(request):
     operacao = None
 
     if request.method == "POST":
-        num1 = float(request.POST.get("num1"))
-        num2 = float(request.POST.get("num2"))
+        numero1 = float(request.POST.get("num1"))
+        numero2 = float(request.POST.get("num2"))
         operacao = request.POST.get("operacao")
 
         if operacao == "adicionar":
-            resultado = num1 + num2
+            resultado = numero1 + numero2
         elif operacao == "subtrair":
-            resultado = num1 - num2
+            resultado = numero1 - numero2
         elif operacao == "multiplicar":
-            resultado = num1 * num2
+            resultado = numero1 * numero2
         elif operacao == "dividir":
             if num2 != 0:
-                resultado = num1 / num2
+                resultado = numero1 / numero2
             else:
                 resultado = "Erro: Divisão por zero"
 
